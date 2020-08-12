3ada8ef6c33d395ba6cc7b66fe2aa50a7789da4b
yonikosiner <yoni@kosiner.co.uk>
2020-08-12 13:33:03 +0100

    moving folder around | adding a my projects pain

---
+++ src/java script/projectjquery.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $("#hamburger").click(function () {
+      $(".hamburger").toggleClass("is-opened");
+      $(".inside").fadeToggle();
+      $(".all").fadeToggle();
+    });
+  });
