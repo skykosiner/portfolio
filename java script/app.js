b9961059fb181590da2b28d322b01cfa102e1f8c
yonikosiner <yoni@kosiner.co.uk>
2020-08-24 00:29:37 +0100

    adding new things to my home automation page

--- java script/app.js
+++ java script/app.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-    $("#hamburger").click(function () {
-      $(".hamburger").toggleClass("is-opened");
-      $(".inside").fadeToggle();
-      $(".hello").fadeToggle();
-      $(".list").fadeToggle();
-      $(".logo").fadeToggle();
-      $(".social").fadeToggle();
-      $(".me").fadeToggle();
-      $("body, html").animate({ scrollTop: 0 });
-      $(".inside").animate({ scrollTop: 69});
-    });
-  });
-  
-  $(window).scroll(function() {
+  $("#hamburger").click(function () {
+    $(".hamburger").toggleClass("is-opened");
+    $(".inside").fadeToggle();
+    $(".hello").fadeToggle();
+    $(".list").fadeToggle();
+    $(".logo").fadeToggle();
+    $(".social").fadeToggle();
+    $(".me").fadeToggle();
     $("body, html").animate({ scrollTop: 0 });
+    $(".inside").animate({ scrollTop: 69 });
   });
+});
+
+$(window).scroll(function () {
+  $("body, html").animate({ scrollTop: 0 });
+});
--- java script/automation.js
+++ java script/automation.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
-    $("#hamburger").click(function () {
-      $(".hamburger").toggleClass("is-opened");
-      $(".inside").fadeToggle();
-      $(".logo").fadeToggle();
-      $(".inside").animate({ scrollTop: 69});
-    });
-  }); 
+  $("#hamburger").click(function () {
+    $(".hamburger").toggleClass("is-opened");
+    $(".inside").fadeToggle();
+    $(".logo").fadeToggle();
+    $(".inside").animate({ scrollTop: 69 });
+  });
+});
+
+$(document).ready(function () {
+  $(".home-kit").click(function () {
+    $(".home-text").fadeToggle();
+  });
+});
