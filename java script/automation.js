a8ee70af76ec925b3e0373cf1c04b0a9dbb560ad
yonikosiner <yoni@kosiner.co.uk>
2020-08-24 18:56:15 +0100

    home atuomation page
    finshing mockups for home automnation page. working on the code now to get paragraph mroe to the bottom when i press one of them

--- java script/automation.js
+++ java script/automation.js
@@ -1,3 +1,4 @@
+// For hamburger menu
 $(document).ready(function () {
   $("#hamburger").click(function () {
     $(".hamburger").toggleClass("is-opened");
@@ -6,9 +7,20 @@ $(document).ready(function () {
     $(".inside").animate({ scrollTop: 69 });
   });
 });
-
+// Get home kit text appear when i press home kit
 $(document).ready(function () {
   $(".home-kit").click(function () {
     $(".home-text").fadeToggle();
+    $(".Multi-room-music").fadeToggle();
+    $(".google").fadeToggle();
+    $(".alexa").fadeToggle();
   });
+});
+// Get div automation to move when apple home kit is clicked
+$(document).ready(function () {
+  if ($(".home-text").css("display") == ("block")) {
+    alert("The paragraph  is visible.");
+  } else {
+    alert("The paragraph  is hidden.");
+  }
 });
