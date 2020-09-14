adccba6f782819c22711679ebc2640c0f1abcbea
yonikosiner <yoni@kosiner.co.uk>
2020-09-14 07:55:16 +0100

    install commit

--- java script/automation.js
+++ java script/automation.js
@@ -1,3 +1,4 @@
+// For hamburger menu
 $(document).ready(function () {
   $("#hamburger").click(function () {
     $(".hamburger").toggleClass("is-opened");
@@ -7,8 +8,23 @@ $(document).ready(function () {
   });
 });
 
+// All for  home kit button
+// Get home kit text appear when i press home kit
 $(document).ready(function () {
   $(".home-kit").click(function () {
     $(".home-text").fadeToggle();
+    $(".Multi-room-music").fadeToggle();
+    $(".google").fadeToggle();
+    $(".alexa").fadeToggle();
   });
 });
+//  When user scroll's after the being of the home description` 1
+window.onscroll = function (e) {
+  var x = document.getElementById("third-paragraph");
+  if (window.getComputedStyle(x).display == "none") {
+    x.style.display = "block";
+    $(".first-paragraph").fadeToggle();
+    $(".second-paragraph").fadeToggle();
+  }
+}
+// ALl for alexa button
